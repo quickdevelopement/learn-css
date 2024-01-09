228c81a9087acb7b62b0ca95ee5876a9703c5bbd
ariful4082 <ariful4082@gmail.com>
2024-01-09 23:58:01 +0600

    responsive menu for any device

---
+++ script.js
@@ -0,0 +1,5 @@
+var btnBar = document.getElementById("bar");
+btnBar.addEventListener("click", function(){
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("toggleMenu");
+})
